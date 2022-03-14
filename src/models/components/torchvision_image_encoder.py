ac5a98bc10b8793e7f300ae1a8ab1e5c9570b73b
kam woh <kamwoh@gmail.com>
2022-03-14 14:39:21 +0000

    added vit encoder

--- src/models/components/torchvision_image_encoder.py
+++ src/models/components/torchvision_image_encoder.py
@@ -1,25 +1,46 @@
+import timm
 import torch
 import torch.nn as nn
 import torchvision
 
 
+def get_vit(name, pretrained=True, **kwargs):
+    names = [
+        'vit_base_patch16_224',
+        'vit_tiny_patch16_224',
+        'vit_small_patch16_224',
+        'vit_large_patch16_224'
+    ]
+    assert name in names
+    model = timm.create_model(name, pretrained=pretrained,
+                              drop_rate=kwargs.get('drop_rate', 0.),
+                              attn_drop_rate=kwargs.get('attn_drop_rate', 0.),
+                              drop_path_rate=kwargs.get('drop_path_rate', 0.))
+    return model
+
+
 class TorchvisionImageEncoder(nn.Module):
     def __init__(
-        self,
-        name: str = "resnet50",
-        pretrained: bool = True,
-        zero_init_residual: bool = False,
-        num_output_features: int = 1,
-        pool_type: str = "avg",
+            self,
+            name: str = "resnet50",
+            pretrained: bool = True,
+            zero_init_residual: bool = False,
+            num_output_features: int = 1,
+            pool_type: str = "avg",
     ):
         super().__init__()
 
-        model = getattr(torchvision.models, name)(
-            pretrained=pretrained, zero_init_residual=zero_init_residual
-        )
-        modules = list(model.children())[:-2]
-        self.model = nn.Sequential(*modules)
-        self.pool = self._pool_func(pool_type, num_output_features)
+        self.is_vit = 'vit' in name
+        if self.is_vit:
+            model = get_vit(name, pretrained=pretrained)
+            self.model = model
+        else:
+            model = getattr(torchvision.models, name)(
+                pretrained=pretrained, zero_init_residual=zero_init_residual
+            )
+            modules = list(model.children())[:-2]
+            self.model = nn.Sequential(*modules)
+            self.pool = self._pool_func(pool_type, num_output_features)
 
     def _pool_func(self, pool_type, num_output_features):
         pool_func = nn.AdaptiveAvgPool2d if pool_type == "avg" else nn.AdaptiveMaxPool2d
@@ -40,6 +61,9 @@ class TorchvisionImageEncoder(nn.Module):
         return pool
 
     def forward(self, x):
-        out = self.pool(self.model(x))
-        out = torch.flatten(out, start_dim=1)
+        if self.is_vit:
+            out = self.model(x)  # (bs, dim)
+        else:
+            out = self.pool(self.model(x))
+            out = torch.flatten(out, start_dim=1)
         return out
