67cf7c8f5b4cb6dd9a64b78e2e5bda6011720ce0
BrandonHanx <brandon.hxiao@gmail.com>
2022-03-16 18:41:15 +0000

    [feat] add class balanced loss

--- src/models/components/losses.py
+++ src/models/components/losses.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -45,7 +46,7 @@ class ContrastiveLoss(nn.Module):
 
 class MarginSoftmaxLoss(nn.Module):
     def __init__(self, s: float = 8, m: float = 0.2, m_type: str = "cos", **kwargs):
-        super(MarginSoftmaxLoss, self).__init__()
+        super().__init__()
         self.s = s
         self.m = m
         self.m_type = m_type
@@ -72,3 +73,83 @@ class MarginSoftmaxLoss(nn.Module):
 
     def extra_repr(self) -> str:
         return "s={}, m={}, m_type:{}".format(self.s, self.m, self.m_type)
+
+
+def focal_loss(labels, logits, alpha, gamma):
+    """Compute the focal loss between `logits` and the ground truth `labels`.
+    Focal loss = -alpha_t * (1-pt)^gamma * log(pt)
+    where pt is the probability of being classified to the true class.
+    pt = p (if true class), otherwise pt = 1 - p. p = sigmoid(logit).
+    Args:
+      labels: A float tensor of size [batch, num_classes].
+      logits: A float tensor of size [batch, num_classes].
+      alpha: A float tensor of size [batch_size]
+        specifying per-example weight for balanced cross entropy.
+      gamma: A float scalar modulating loss from hard and easy examples.
+    Returns:
+      focal_loss: A float32 scalar representing normalized total loss.
+    """
+    BCLoss = F.binary_cross_entropy_with_logits(input=logits, target=labels, reduction="none")
+
+    if gamma == 0.0:
+        modulator = 1.0
+    else:
+        modulator = torch.exp(
+            -gamma * labels * logits - gamma * torch.log(1 + torch.exp(-1.0 * logits))
+        )
+
+    loss = modulator * BCLoss
+
+    weighted_loss = alpha * loss
+    focal_loss = torch.sum(weighted_loss)
+
+    focal_loss /= torch.sum(labels)
+    return focal_loss
+
+
+class ClassBalancedLoss(nn.Module):
+    """Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
+    Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
+    where Loss is one of the standard losses used for Neural Networks.
+    Args:
+      labels: A int tensor of size [batch].
+      logits: A float tensor of size [batch, no_of_classes].
+      samples_per_cls: A python list of size [no_of_classes].
+      no_of_classes: total number of classes. int
+      loss_type: string. One of "sigmoid", "focal", "softmax".
+      beta: float. Hyperparameter for Class balanced loss.
+      gamma: float. Hyperparameter for Focal loss.
+    Returns:
+      cb_loss: A float tensor representing class balanced loss
+    """
+
+    def __init__(self, samples_per_cls, no_of_classes, loss_type="focal", beta=0.9999, gamma=2.0):
+        super().__init__()
+        effective_num = 1.0 - np.power(beta, samples_per_cls)
+        weights = (1.0 - beta) / np.array(effective_num)
+        self.weights = weights / np.sum(weights) * no_of_classes
+        self.no_of_classes = no_of_classes
+        self.loss_type = loss_type
+        self.gamma = gamma
+
+    def forward(self, logits, labels):
+
+        labels_one_hot = F.one_hot(labels, self.no_of_classes).float()
+
+        weights = torch.tensor(self.weights).float().to(logits.device)
+        weights = weights.unsqueeze(0)
+        weights = weights.repeat(labels_one_hot.shape[0], 1) * labels_one_hot
+        weights = weights.sum(1)
+        weights = weights.unsqueeze(1)
+        weights = weights.repeat(1, self.no_of_classes)
+
+        if self.loss_type == "focal":
+            cb_loss = focal_loss(labels_one_hot, logits, weights, self.gamma)
+        elif self.loss_type == "sigmoid":
+            cb_loss = F.binary_cross_entropy_with_logits(
+                input=logits, target=labels_one_hot, weight=weights
+            )
+        elif self.loss_type == "softmax":
+            pred = logits.softmax(dim=1)
+            cb_loss = F.binary_cross_entropy(input=pred, target=labels_one_hot, weight=weights)
+        return cb_loss
