5e0abf095f02da7d8640399a30d08587be7f4471
BrandonHanx <brandon.hxiao@gmail.com>
2022-04-04 22:59:19 +0100

    [feat] add multilabel baseline

--- src/models/ebay_module.py
+++ src/models/ebay_module.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from pytorch_lightning import LightningModule
 from torch.optim.lr_scheduler import MultiStepLR
-from torchmetrics import MaxMetric
+from torchmetrics import F1Score, MaxMetric
 from torchmetrics.classification.accuracy import Accuracy
 from torchvision.transforms import RandomChoice
 
@@ -671,3 +671,150 @@ class eBayMoSupConModule(eBaySupConModule):
         self.log("train/supcon_loss", supcon_loss, on_step=True, on_epoch=True, prog_bar=False)
 
         return {"loss": supcon_loss}
+
+
+class eBayMultiModule(LightningModule):
+    def __init__(
+        self,
+        net: torch.nn.Module,
+        lr: float = 0.001,
+        weight_decay: float = 0.0005,
+        output_dim: int = 2048,
+        warmup_steps: int = -1,
+        milestones: List[int] = None,
+        classifier_lr_multiplier: float = 1.0,
+        optimizer: str = "adamw",
+        **kwargs
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # it also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.net = net
+        self.linear = nn.Linear(output_dim, 22295, bias=False)
+
+        # loss function
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+
+        # use separate metric instance for train, val and test step
+        # to ensure a proper reduction over the epoch
+        self.train_acc = F1Score()
+        self.val_acc = F1Score()
+
+        # for logging best so far validation accuracy
+        self.val_acc_best = MaxMetric()
+
+    def forward(self, x: torch.Tensor):
+        return self.net(x)
+
+    def step(self, batch: Any):
+        x = batch["image"]
+        y = batch["multi_label"]
+
+        feats = self.forward(x)
+        logits = self.linear(feats)
+        loss = self.criterion(logits, y)
+
+        preds = torch.sigmoid(logits)
+        return loss, preds, y.long()
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log train metrics
+        acc = self.train_acc(preds, targets)
+        self.log("train/loss", loss, on_step=True, on_epoch=True, prog_bar=False)
+        self.log("train/acc", acc, on_step=True, on_epoch=True, prog_bar=True)
+
+        # we can return here dict with any tensors
+        # and then read it in some callback or in `training_epoch_end()`` below
+        # remember to always return loss from `training_step()` or else backpropagation will fail!
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def training_epoch_end(self, outputs: List[Any]):
+        # `outputs` is a list of dicts returned from `training_step()`
+        pass
+
+    def validation_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log val metrics
+        acc = self.val_acc(preds, targets)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def validation_epoch_end(self, outputs: List[Any]):
+        acc = self.val_acc.compute()  # get val accuracy from current epoch
+        self.val_acc_best.update(acc)
+        self.log("val/acc_best", self.val_acc_best.compute(), on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch: Any, batch_idx: int):
+        pass
+
+    def test_epoch_end(self, outputs: List[Any]):
+        pass
+
+    def predict_step(self, batch: Any, batch_idx: int, dataloader_idx: int):
+        feats = self.forward(batch["image"])
+        return {"feats": feats, "uuid": batch["uuid"]}
+
+    def on_epoch_end(self):
+        # reset metrics at the end of every epoch
+        self.train_acc.reset()
+        self.val_acc.reset()
+
+    def get_params(self):
+        net_params = get_configured_parameters(
+            self.net,
+            base_lr=self.hparams.lr,
+            weight_decay=self.hparams.weight_decay,
+        )
+        linear_params = get_configured_parameters(
+            self.linear,
+            base_lr=self.hparams.lr,
+            weight_decay=self.hparams.weight_decay,
+            lr_multiplier=self.hparams.classifier_lr_multiplier,
+        )
+        return net_params + linear_params
+
+    def configure_optimizers(self):
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        See examples here:
+            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+        """
+        if self.hparams.optimizer == "adam":
+            optimizer = torch.optim.Adam(params=self.get_params())
+        elif self.hparams.optimizer == "adamw":
+            optimizer = torch.optim.AdamW(params=self.get_params())
+        elif self.hparams.optimizer == "sgd":
+            optimizer = torch.optim.SGD(params=self.get_params(), momentum=0.9)
+        if self.hparams.milestones is not None:
+            lr_scheduler = MultiStepLR(optimizer, self.hparams.milestones)
+            return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
+        return {"optimizer": optimizer}
+
+    def optimizer_step(
+        self,
+        epoch,
+        batch_idx,
+        optimizer,
+        optimizer_idx,
+        optimizer_closure,
+        on_tpu,
+        using_native_amp,
+        using_lbfgs,
+    ):
+        # update params
+        optimizer.step(closure=optimizer_closure)
+
+        # manually warm up lr without a scheduler
+        if self.trainer.global_step < self.hparams.warmup_steps:
+            lr_scale = min(1.0, float(self.trainer.global_step + 1) / self.hparams.warmup_steps)
+            for pg in optimizer.param_groups:
+                pg["lr"] = lr_scale * self.hparams.lr
